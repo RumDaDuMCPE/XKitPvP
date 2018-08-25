779a40924d475ee359b865707b449342dd5ad3a6
RumBenCraftPE <ameensahal321@gmail.com>
2018-08-25 23:36:11 +0300

    Added Portals

--- src/KitPvP/regions/SpawnRegion.php
+++ src/KitPvP/regions/SpawnRegion.php
@@ -8,6 +8,21 @@ use pocketmine\Player;
 
 class SpawnRegion {
 
+    private $portals = [
+        0 => ['x' => [231,233], 'z' => [260]],
+        1 => ['x' => [279,281], 'z' => [252]],
+        2 => ['x' => [260], 'z' => [279,281]],
+        3 => ['x' => [252], 'z' => [279,281]],
+        4 => ['x' => [252], 'z' => [279,281]],
+        5 => ['z' => [232,234], 'x' => [260]]
+    ];
+
+
+    /*
+     * NOTE: Player class is an instance of Position.
+     *       Player -> Human > Creature > Living > Entity -> Position.
+     */
+
     /**
      * @param Player $vector3
      * @return bool
@@ -23,8 +38,28 @@ class SpawnRegion {
         return false;
     }
 
-    public function inAnyArena(Player $vector3) : bool {
-
+    /**
+     * @param Player $vector3
+     * @return bool
+     */
+    public function enteringPortal(Player $vector3) : bool
+    {
+        $y = [0, 700];
+        for ($i = 0; $i < count($this->portals); $i++) {
+            $x = $this->portals[$i]['x'];
+            $z = $this->portals[$i]['z'];
+            $x = min($x);
+            $X = max($x);
+            $z = min($z);
+            $Z = max($z);
+            $axis = [new AxisAlignedBB($x, $X, min($y), max($y), $z, $Z)];
+            for ($i = 0; $i < count($axis); $i++) {
+                if ($axis[$i]->isVectorInside($vector3)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
 }
